63535faed5f8ee05e5be68bf460f4572f6ca9498
AkashdeepArjun <wadhwaarjun007@gmail.com>
2024-06-21 15:29:26 +0530

    fixed password encryption

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,3 +1,6 @@
+
+'use server'
+
 import connect from '@/dbConfig/db_config'
 
 import User from '@/models/userModel.js'
@@ -6,6 +9,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import bcryptjs from "bcryptjs"
 import { Sedgwick_Ave_Display } from 'next/font/google'
+import { hash } from 'crypto'
 
 connect()
 
@@ -17,7 +21,7 @@ export async function POST(req: NextRequest) {
 
     const req_body = await req.json();
 
-    const { username, email, password } = req_body;
+    const { username, email, password: pswd } = req_body;
 
 
     console.log("request being sent is ", req_body);
@@ -30,20 +34,27 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "user already exist" }, { status: 400 });
     }
 
-    const salt = await bcryptjs.genSalt(10);
 
-    const hashed_passwd = await bcryptjs.hash(password, salt);
+    let salt = bcryptjs.genSaltSync()
 
 
-    const new_user = new User({ username, email, password })
+    let password = bcryptjs.hashSync(pswd, salt)
+
+
+
+
+
+    if (password != null) {
+      console.log(`FOOKING password is ${password}`);
+      console.log("TYPE OF passoword is ", typeof password);
+
+    }
+
+    const new_user = new User({ 'username': username, 'email': email, 'password': password })
 
 
 
     const saved_user = await new_user.save()
-
-
-
-
     return NextResponse.json({ message: "user saved success", yikes: saved_user }, { status: 201 })
 
   } catch (error: any) {
